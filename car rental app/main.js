6af1c8286508f35ba8868d8cc048cc0acd53da42
piotrromatowski <59802218+piotrromatowski@users.noreply.github.com>
2021-03-29 18:00:15 +0200

    Delete car rental app directory

--- car rental app/main.js
+++
@@ -1,6 +0,0 @@
-// const btn = document.querySelector(".burger");
-const btn = document.getElementById("burger");
-
-btn.addEventListener("click", () => {
-  btn.classList.toggle("burger-active");
-});
